c8e00f0492323f242e6d2cb2d79e9ca124cfeade
chindhu Rathnasamy <130512054+chindhur@users.noreply.github.com>
2023-04-13 21:48:46 -0700

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,12 +1,15 @@
 public class InsertionSort {
     /*Function to sort array using insertion sort*/
-    void sort(int arr[])
+    void sort(int[] arr)
     {
         int n = arr.length;
         for (int i = 1; i < n; ++i) {
             int key = arr[i];
             int j = i - 1;
-
+            
+ /* Move elements that are greater than key, to one position ahead
+               of their current position */
+            
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
@@ -15,8 +18,8 @@ public class InsertionSort {
         }
     }
 
-
-    static void printArray(int arr[])
+/*prints the array*/
+    static void printArray(int[] arr)
     {
         int n = arr.length;
         for (int i = 0; i < n; ++i)
@@ -25,10 +28,10 @@ public class InsertionSort {
         System.out.println();
     }
 
-    // Driver method
-    public static void main(String args[])
+    /* Driver method */
+    public static void main(String[] args)
     {
-        int arr[] = { 12, 11, 13, 5, 6 ,56,43,1,21};
+        int[] arr = { 12, 11, 13, 5, 6 ,56,43,1,21};
 
         InsertionSort ob = new InsertionSort();
         ob.sort(arr);
