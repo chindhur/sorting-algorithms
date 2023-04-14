412cba124c67a6f266c1864d8bd802784dc5d9a1
chindhu Rathnasamy <130512054+chindhur@users.noreply.github.com>
2023-04-13 22:12:15 -0700

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,13 +1,14 @@
 public class SelectionSort
 {
+    /*function to sort an array using selection sort*/
     void sort(int[] arr)
     {
         int n = arr.length;
 
-        //To move unsorted subarray one after the other
+        /*To move unsorted subarray one after the other*/
         for (int i = 0; i < n-1; i++)
         {
-            // To Find the minimum element in unsorted array
+            /* To Find the minimum element in unsorted array*/
             int min = i;
             for (int j = i+1; j < n; j++) {
                 if (arr[j] < arr[min]) {
@@ -15,14 +16,14 @@ public class SelectionSort
                 }
             }
 
-            // Swap the minimum element with the first element
+            /* Swap the minimum element with the first element*/
             int temp = arr[min];
             arr[min] = arr[i];
             arr[i] = temp;
         }
     }
 
-    // Prints the array
+    /* Prints the array*/
     void printArray(int[] arr)
     {
         int n = arr.length;
@@ -35,7 +36,7 @@ public class SelectionSort
     public static void main(String[] args)
     {
         SelectionSort s = new SelectionSort();
-        int[] arr = {56,89,35,12,1,84,21};
+        int[] arr = {92,1,84,21,67,88,38};
         s.sort(arr);
         System.out.println("Sorted array");
         s.printArray(arr);
