76e33bf9a2641dc6cbcc8d9240f370632c384b0b
chindhu Rathnasamy <130512054+chindhur@users.noreply.github.com>
2023-04-13 17:58:10 -0700

    Update HelloUser.java

--- HelloUser.java
+++ HelloUser.java
@@ -1,9 +1,9 @@
 public class HelloUser{
     public static void main(String[] args){
-       // Checking if length of args array is > 0
+        // Checking if length of args array is > 0
         if(args.length>0){
             System.out.print("Hello \n");
-  //Iterating args with for each loop
+            //Iterating args with for each loop
             for(String name:args){
                 //printing command line arguments
                 System.out.print(name + ' ');
